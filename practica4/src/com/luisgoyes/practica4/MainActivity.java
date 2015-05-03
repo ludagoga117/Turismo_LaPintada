eb75b5bda3fa3a87d1c5140d8f0e854124d04ce2
Luis David Goyes <luis.goyes@udea.edu.co>
2015-05-03 17:12:08 -0500

    avance

--- practica4/src/com/luisgoyes/practica4/MainActivity.java
+++ practica4/src/com/luisgoyes/practica4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.luisgoyes.practica4;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,20 +17,68 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch(item.getItemId()){
+			case R.id.iHoteles:
+				HotelesMenuItem();
+				break;
+			case R.id.iBares:
+				BaresMenuItem();
+				break;
+			case R.id.iTurista:
+				TuristaMenuItem();
+				break;
+			case R.id.iDemografica:
+				DemograficaMenuItem();
+				break;
+			case R.id.iAbout:
+				AboutMenuItem();
+				break;
+			case R.id.iHelp:
+				HelpMenuItem();
+				break;
+			default:
+				break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void HotelesMenuItem(){
+		
+	}
+	
+	private void BaresMenuItem(){
+		
+	}
+	
+	private void TuristaMenuItem(){
+		
+	}
+	
+	private void DemograficaMenuItem(){
+		
+	}
+	
+	private void AboutMenuItem(){
+		new AlertDialog.Builder(this)
+		.setTitle(getResources().getString(R.string.sAbout))
+		.setMessage(getResources().getString(R.string.sAboutInfo))
+		.setNeutralButton(getResources().getString(R.string.sOk), new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+		}).show();
+	}
+	
+	private void HelpMenuItem(){
+		
 	}
 }
